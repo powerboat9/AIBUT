6adc2095d5ff7686f3eba06d33373bd7bfa856f3
powerboat9 <powerboat9@users.noreply.github.com>
2015-08-20 15:35:06 -0400

    Create behaviour.lua

---
+++ AI/behaviour.lua
@@ -0,0 +1,8 @@
+os.loadAPI("AI/config")
+
+isWild = config.isWild
+type = config.type
+allowBlockBreak = config.allowBlockBreak
+allowEntityKill = config.allowEntityKill
+
+os.unloadAPI("AI/config")
