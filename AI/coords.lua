797ebec220f72f680f299f3915185ad552d3b0e6
powerboat9 <powerboat9@users.noreply.github.com>
2015-08-20 17:12:19 -0400

    Update coords.lua

--- AI/coords.lua
+++ AI/coords.lua
@@ -1,6 +1,10 @@
 function getFacingCoords(direction, x, y, z)
     if type(direction) == "string" then
         directionLow = string.lower(direction)
+        if directionLow == "east" then
+            return x + 1, y, z
+        elseif directionLow == "west" then
+            return x - 1, y, z
         elseif directionLow == "up" then
             return x, y + 1, z
         elseif directionLow == "down" then
