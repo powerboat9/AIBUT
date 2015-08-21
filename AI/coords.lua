b75445f417a9fb4582f85df7f1451ee93f2c206c
powerboat9 <powerboat9@users.noreply.github.com>
2015-08-20 20:49:32 -0400

    Update coords.lua

--- AI/coords.lua
+++ AI/coords.lua
@@ -16,26 +16,26 @@ function getFacingCoords(direction, x, y, z)
         directionLow = getSides()[direction]
         if not direction then
             error("Invalid Direction ID", 2)
-    end
-    
-    if type(direction) == "string" then
-        local directionLow = string.lower(direction)
-        if directionLow == "east" then
-            return x + 1, y, z
-        elseif directionLow == "west" then
-            return x - 1, y, z
-        elseif directionLow == "up" then
-            return x, y + 1, z
-        elseif directionLow == "down" then
-            return x, y - 1, z
-        elseif directionLow == "south" then
-            return x, y, z + 1
-        elseif directionLow == "north" then
-            return x, y, z - 1
-        else
-            error("Invalid Direction: " .. direction, 2)
         end
+    elseif type(direction) == "string" then
+        local directionLow = string.lower(direction)
     else
         error("Invalid Type: " .. type(direction), 2)
+    end
+        
+    if directionLow == "east" then
+        return x + 1, y, z
+    elseif directionLow == "west" then
+        return x - 1, y, z
+    elseif directionLow == "up" then
+        return x, y + 1, z
+    elseif directionLow == "down" then
+        return x, y - 1, z
+    elseif directionLow == "south" then
+        return x, y, z + 1
+    elseif directionLow == "north" then
+        return x, y, z - 1
+    else
+        error("Invalid Direction: " .. direction, 2)
     end
 end
