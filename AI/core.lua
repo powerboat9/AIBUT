4b53c7ff8ea21c37833677a7b796942e29c574d9
powerboat9 <powerboat9@users.noreply.github.com>
2015-11-11 17:34:39 -0500

    Update core.lua

--- AI/core.lua
+++ AI/core.lua
@@ -1,1 +1,29 @@
-function 
+local pendingActions = {}
+
+function runTurtleApi()
+    local action = pendingActions[1]
+    if action.type == "death" then
+        if type(action.data) == string then
+            local death = fs.open("AIBUT/data/flags/dead", w)
+            death.write("true\n" .. action.data)
+            fs.close(death)
+        else
+            fail()
+        end
+    elseif action.type == "movement" then
+        if type(action.data) == "string" then
+            turtle[action.data]()
+        else
+            fail()
+        end
+    elseif action.type == "install subroutine" then
+        if type(action.data) == "table" then
+            table.insert(subroutines, action.data)
+        else
+            fail()
+        end
+    else
+        fail()
+    end
+    table.remove(pendingActions, 1)
+end
