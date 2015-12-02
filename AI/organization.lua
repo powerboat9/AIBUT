aa84e6b79f127d1d651b90cf09b9cc2c287608ba
powerboat9 <powerboat9@users.noreply.github.com>
2015-12-01 20:13:03 -0500

    Update organization.lua

--- AI/organization.lua
+++ AI/organization.lua
@@ -1,7 +1,8 @@
 function moveAllOfType(slot, name)
+    name = name or ((turtle.getItemCount(slot) ~= 0) and turtle.getItemDetail(slot)) or error("Invalid Item Name")
     for i = 1, 16 do
         turtle.select(i)
-        if (turtle.getItemDetail().name == name) and (slot ~= i) then
+        if ((turtle.getItemDetail().name == name) or not name) and (slot ~= i) then
             turtle.transferTo(slot)
         end
     end
@@ -16,13 +17,14 @@ function clearForCache()
             if turtle.getItemCount(i) == 0 then
                 turtle.select(16)
                 turtle.transferTo(i)
+                return 16
             end
         end
     else
-        for i = 16, 1, -1 do
-            turtle.select(i)
-            local amountToMove
-
+        for i = 1, 16 do
+            moveAllOfType(i)
+        end
+    end
 function sort(format)
     local slotContains = {}
     local slotExamine = 1
