eeb2a6dabbe05cd3c2a7618203b40531d04622e6
powerboat9 <powerboat9@users.noreply.github.com>
2016-05-31 22:57:07 -0400

    Rename core.lua to cores.lua

---
+++ AI/cores.lua
@@ -0,0 +1,12 @@
+local function createAction(name, actionList)
+    return function()
+        local i = 1
+        while true do
+            local act = actionList[1]
+end
+
+local function createDrive(reliefActionName, painActionName, funct)
+    return {
+        function 
+
+local function
